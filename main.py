31818541fa5933b566c32b32e1c63dc2d2bd1d23
Temake <teminioluwaopemipo@gmail.com>
2025-10-16 17:39:22 +0100

    fix: The data structure for the profile

--- main.py
+++ main.py
@@ -11,20 +11,28 @@ async def read_root():
 
 @app.get("/me")
 async def read_me():
-    profile = {
-        "name": "Teminioluwa Adekoya",
-        "email": "teminioluwaopemipo@gmail.com",
-        "stack":"Python/FastAPI",
-        "timestamp": datetime.datetime.utcnow().isoformat(),
-        "github": "https://github.com/temake"
-    }
-
     try:
-        async with httpx.AsyncClient(timeout=5.0) as client:
-            resp = await client.get("https://catfact.ninja/fact")
-            resp.raise_for_status()
-            profile["fact"] = resp.json().get("fact", "No fact available.")
-    except httpx.HTTPError:
-        profile["fact"] = "Unable to fetch cat fact at this time."
+        profile = {
+            "user" :{
+            "name": "Teminioluwa Adekoya",
+            "email": "teminioluwaopemipo@gmail.com",
+            "stack":"Python/FastAPI",
+            "linkedIn":"https://www.linkedin.com/in/teminioluwa-adekoya-6537a2294/",
+            "github": "https://github.com/temake"
+            },
+            "timestamp": datetime.datetime.now(datetime.timezone.utc),
+            
+        }
 
-    return profile
+        try:
+            async with httpx.AsyncClient(timeout=5.0) as client:
+                resp = await client.get("https://catfact.ninja/fact")
+                resp.raise_for_status()
+                profile["fact"] = resp.json().get("fact", "No fact available.")
+        except httpx.HTTPError:
+            profile["fact"] = "Unable to fetch cat fact at this time."
+        if profile:
+            profile["status"]= "Success"
+        return profile
+    except Exception as e:
+        print(f"An Error Occured : {e}")
