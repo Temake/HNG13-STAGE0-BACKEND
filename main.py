55a2568b06f2557bf3b30c02311cf8ae0e8903bb
Temake <teminioluwaopemipo@gmail.com>
2025-10-16 16:54:48 +0100

    fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import httpx
 
+import datetime
 app = FastAPI()
 
 
@@ -12,8 +13,9 @@ async def read_root():
 async def read_me():
     profile = {
         "name": "Teminioluwa Adekoya",
-        "role": "Backend Engineer",
-        "location": "Lagos, Nigeria",
+        "email": "teminioluwaopemipo@gmail.com",
+        "stack":"Python/FastAPI",
+        "timestamp": datetime.datetime.utcnow().isoformat(),
         "github": "https://github.com/temake"
     }
 
@@ -21,8 +23,8 @@ async def read_me():
         async with httpx.AsyncClient(timeout=5.0) as client:
             resp = await client.get("https://catfact.ninja/fact")
             resp.raise_for_status()
-            profile["cat_fact"] = resp.json().get("fact", "No fact available.")
+            profile["fact"] = resp.json().get("fact", "No fact available.")
     except httpx.HTTPError:
-        profile["cat_fact"] = "Unable to fetch cat fact at this time."
+        profile["fact"] = "Unable to fetch cat fact at this time."
 
     return profile
